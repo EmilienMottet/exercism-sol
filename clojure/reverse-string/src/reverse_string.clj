d395570775ade7ad40fc77f4864e7e2aeb4b8081
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-12-06 14:14:25 +0100

    use into with list to reverse string

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,8 +1,3 @@
 (ns reverse-string)
 
-(defn reverse-string
-  ([s] (apply str (reverse-string s '())))
-  ([a acc]
-   (if (empty? a)
-     acc
-     (recur (rest a) (conj acc (first a))))))
+(defn reverse-string [s] (apply str (into () s)))
