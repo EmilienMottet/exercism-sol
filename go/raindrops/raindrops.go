8df8b7cd98372961c652b860ab07c9aae040d0ff
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-12-10 15:21:15 +0100

    sort key to assure order

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,14 +1,24 @@
 package raindrops
 
-import "strconv"
+import (
+	"sort"
+	"strconv"
+)
 
 // Convert an integer into a raindrops
 func Convert(n int) string {
-	rains := map[int]string{3: "Pling", 5: "Plang", 7: "Plong"}
 	res := ""
-	for k, v := range rains {
+	rains := map[int]string{3: "Pling", 5: "Plang", 7: "Plong"}
+	keys := make([]int, len(rains))
+	i := 0
+	for k, _ := range rains {
+		keys[i] = k
+		i++
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
 		if n%k == 0 {
-			res += v
+			res += rains[k]
 		}
 	}
 
