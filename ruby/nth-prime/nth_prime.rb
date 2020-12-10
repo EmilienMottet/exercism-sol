89ced7289165eaefab1b15f6ed7d0e9dbe64402f
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-12-11 00:27:28 +0100

    add nth_prime

---
+++ ruby/nth-prime/nth_prime.rb
@@ -0,0 +1,11 @@
+class Prime
+  def self.prime?(n)
+    (2...(n / 2 + 1)).none? { |i| (n % i).zero? }
+  end
+
+  def self.nth(n)
+    raise ArgumentError if n < 1
+
+    (2..).lazy.filter { |i| is_prime?(i) }.first(n)[-1]
+  end
+end
