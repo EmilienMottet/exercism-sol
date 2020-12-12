b27534f7e8eb468c1e8a99a4b2ec740d6d5109d7
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-12-12 22:46:24 +0100

    refactor triangle

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -1,30 +1,30 @@
 class Triangle {
-  private final double s1, s2, s3;
+  private final double side1, side2, side3;
 
-  private static boolean inequality(final double s1, final double s2, final double s3) {
-    return s1 < s2 + s3 && s2 < s1 + s3 && s3 < s1 + s2;
+  private static boolean isValidTriangle(final double side1, final double s2, final double s3) {
+    return side1 < s2 + s3 && s2 < side1 + s3 && s3 < side1 + s2;
   }
 
   Triangle(final double side1, final double side2, final double side3) throws TriangleException {
-    if (!inequality(side1, side2, side3)) {
+    if (!isValidTriangle(side1, side2, side3)) {
       throw new TriangleException();
     }
-    s1 = side1;
-    s2 = side2;
-    s3 = side3;
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
 
   }
 
   boolean isEquilateral() {
-    return s1 == s2 && s2 == s3 && s1 == s3;
+    return side1 == side2 && side2 == side3;
   }
 
   boolean isIsosceles() {
-    return s1 == s2 || s2 == s3 || s1 == s3;
+    return side1 == side2 || side2 == side3 || side1 == side3;
   }
 
   boolean isScalene() {
-    return !isEquilateral() && !isIsosceles();
+    return !isIsosceles();
   }
 
 }
