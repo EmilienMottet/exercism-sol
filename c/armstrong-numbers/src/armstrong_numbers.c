e291ef4e34a503e6b9096c0dce967eef53e05586
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-12-16 22:10:01 +0100

    use for instead of while

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,17 +1,16 @@
 #include "armstrong_numbers.h"
 #include <math.h>
+#include <stdio.h>
 
 bool is_armstrong_number(int candidate) {
   if (candidate == 0) {
     return true;
   }
 
-  int acc = candidate / 10, d = candidate % 10, power = log10(candidate) + 1,
-      sum = pow(d, power);
-  while (acc != 0) {
-    d = acc % 10;
-    acc /= 10;
-    sum += pow(d, power);
+  int sum = 0, power = log10(candidate) + 1;
+
+  for (int acc = candidate, d = 0; acc != 0;
+       d = acc % 10, acc /= 10, sum += pow(d, power)) {
   }
 
   return sum == candidate;
