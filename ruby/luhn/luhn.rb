b677cd716dd6befb3a1bc209b595c94032b0a20a
EmilienMottet <emilien.mottet@grenoble-inp.org>
2021-03-16 22:30:07 +0100

    update luhn and user ruby 3.0

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,28 +1,35 @@
-# frozen_string_literal: true
-
 # Luhn algorithm is a simple checksum formula used to validate a variety of identification numbers,
 # such as credit card numbers and Canadian Social Insurance
 class Luhn
-  def self.checksum(arr)
-    arr.reverse.map.with_index.sum do |v, i|
-      (if i.even?
-         v
-       elsif v < 5
-         v * 2
-       else
-         (v * 2) - 9
-       end)
+  class <<
+      
+    private
+
+    def checksum(identification_number)
+      identification_number.reverse.map.with_index.sum do |digit, index|
+        (if index.even?
+           digit
+         elsif digit < 5
+           digit * 2
+         else
+           (digit * 2) - 9
+         end)
+      end
+    end
+
+    def normalized_and_validate?(identification_number)
+      0 == checksum(identification_number.chars.map(&:to_i)) % 10
+    end
+
+    public
+
+    def valid?(identification_number)
+      identification_number = identification_number.gsub(' ', '')
+
+      return false if identification_number =~ /\D/
+      return false if identification_number.length < 2
+
+      normalized_and_validate?(identification_number)
     end
   end
-
-  def self.valid?(str)
-    str.gsub!(' ', '')
-
-    return false unless str !~ /\D/
-    return false if str.length < 2
-
-    (checksum(str.chars.map(&:to_i)) % 10).zero?
-  end
-
-  private_class_method :checksum
 end
