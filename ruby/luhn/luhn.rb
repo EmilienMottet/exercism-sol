52d88571a778fe390302644946d3a35b67954698
EmilienMottet <emilien.mottet@grenoble-inp.org>
2021-03-17 20:39:27 +0100

    refacto luhn ruby

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,24 +1,32 @@
 # Luhn algorithm is a simple checksum formula used to validate a variety of identification numbers,
 # such as credit card numbers and Canadian Social Insurance
 class Luhn
-  class <<
-      
+  class << self
     private
 
     def checksum(identification_number)
       identification_number.reverse.map.with_index.sum do |digit, index|
-        (if index.even?
-           digit
-         elsif digit < 5
-           digit * 2
-         else
-           (digit * 2) - 9
-         end)
+        if index.even?
+          digit
+        elsif digit < 5
+          digit * 2
+        else
+          (digit * 2) - 9
+        end
       end
     end
 
+    def normalized(identification_number)
+      identification_number.chars.map(&:to_i)
+    end
+
+    def validate?(normalized_id_number)
+      (checksum(normalized_id_number) % 10).zero?
+    end
+
     def normalized_and_validate?(identification_number)
-      0 == checksum(identification_number.chars.map(&:to_i)) % 10
+      normalized_id_number = normalized(identification_number)
+      validate?(normalized_id_number)
     end
 
     public
