7a7d2f5d37175b02e0d638d0a79293d55233bce3
EmilienMottet <emilien.mottet@grenoble-inp.org>
2021-03-18 20:29:24 +0100

    refacto luhn ruby v2

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,18 +1,25 @@
-# Luhn algorithm is a simple checksum formula used to validate a variety of identification numbers,
+# Luhn algorithm is a simple checksum formula used to check a variety of identification numbers,
 # such as credit card numbers and Canadian Social Insurance
 class Luhn
+  def self.valid?(identification_number)
+    numbers = identification_number.gsub(' ', '')
+
+    return false if numbers =~ /\D/
+    return false if numbers.length < 2
+
+    normalized_and_check?(numbers)
+  end
+
   class << self
     private
 
     def checksum(identification_number)
       identification_number.reverse.map.with_index.sum do |digit, index|
-        if index.even?
-          digit
-        elsif digit < 5
-          digit * 2
-        else
-          (digit * 2) - 9
-        end
+        next digit if index.even?
+
+        digit *= 2
+        digit -= 9 unless digit < 10
+        digit
       end
     end
 
@@ -20,24 +27,13 @@ class Luhn
       identification_number.chars.map(&:to_i)
     end
 
-    def validate?(normalized_id_number)
+    def check?(normalized_id_number)
       (checksum(normalized_id_number) % 10).zero?
     end
 
-    def normalized_and_validate?(identification_number)
+    def normalized_and_check?(identification_number)
       normalized_id_number = normalized(identification_number)
-      validate?(normalized_id_number)
-    end
-
-    public
-
-    def valid?(identification_number)
-      identification_number = identification_number.gsub(' ', '')
-
-      return false if identification_number =~ /\D/
-      return false if identification_number.length < 2
-
-      normalized_and_validate?(identification_number)
+      check?(normalized_id_number)
     end
   end
 end
