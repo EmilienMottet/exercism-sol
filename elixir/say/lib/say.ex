4cf1e7bdba858eac4e5c2ec590c780bdcacd8cfc
EmilienMottet <emilien.mottet@grenoble-inp.org>
2021-03-18 20:28:53 +0100

    use inline def

--- elixir/say/lib/say.ex
+++ elixir/say/lib/say.ex
@@ -3,113 +3,71 @@ defmodule Say do
   Translate a positive integer into English.
   """
   @spec in_english(integer) :: {atom, String.t()}
-  def in_english(number) when number >= 0 and number < 1_000_000_000_000 do
-    {:ok, do_in_english(number)}
-  end
+  def in_english(number) when number >= 0 and number < 1_000_000_000_000,
+    do: {:ok, do_in_english(number)}
 
-  def in_english(_) do
-    {:error, "number is out of range"}
-  end
+  def in_english(_), do: {:error, "number is out of range"}
 
-  defp do_in_english(80) do
-    "eighty"
-  end
+  defp do_in_english(80), do: "eighty"
 
-  defp do_in_english(50) do
-    "fifty"
-  end
+  defp do_in_english(50), do: "fifty"
 
-  defp do_in_english(40) do
-    "forty"
-  end
+  defp do_in_english(40), do: "forty"
 
-  defp do_in_english(30) do
-    "thirty"
-  end
+  defp do_in_english(30), do: "thirty"
 
-  defp do_in_english(20) do
-    "twenty"
-  end
+  defp do_in_english(20), do: "twenty"
 
-  defp do_in_english(14) do
-    "fourteen"
-  end
+  defp do_in_english(14), do: "fourteen"
 
-  defp do_in_english(12) do
-    "twelve"
-  end
+  defp do_in_english(12), do: "twelve"
 
-  defp do_in_english(9) do
-    "nine"
-  end
+  defp do_in_english(9), do: "nine"
 
-  defp do_in_english(7) do
-    "seven"
-  end
+  defp do_in_english(7), do: "seven"
 
-  defp do_in_english(6) do
-    "six"
-  end
+  defp do_in_english(6), do: "six"
 
-  defp do_in_english(5) do
-    "five"
-  end
+  defp do_in_english(5), do: "five"
 
-  defp do_in_english(4) do
-    "four"
-  end
+  defp do_in_english(4), do: "four"
 
-  defp do_in_english(3) do
-    "three"
-  end
+  defp do_in_english(3), do: "three"
 
-  defp do_in_english(2) do
-    "two"
-  end
+  defp do_in_english(2), do: "two"
 
-  defp do_in_english(1) do
-    "one"
-  end
+  defp do_in_english(1), do: "one"
 
-  defp do_in_english(0) do
-    "zero"
-  end
+  defp do_in_english(0), do: "zero"
 
-  defp do_in_english(number) when rem(number, 1_000_000_000) == 0 do
-    "#{do_in_english(div(number, 1_000_000_000))} billion"
-  end
+  defp do_in_english(number) when rem(number, 1_000_000_000) == 0,
+    do: "#{do_in_english(div(number, 1_000_000_000))} billion"
 
-  defp do_in_english(number) when rem(number, 1_000_000) == 0 do
-    "#{do_in_english(div(number, 1_000_000))} million"
-  end
+  defp do_in_english(number) when rem(number, 1_000_000) == 0,
+    do: "#{do_in_english(div(number, 1_000_000))} million"
 
-  defp do_in_english(number) when rem(number, 1_000) == 0 do
-    "#{do_in_english(div(number, 1_000))} thousand"
-  end
+  defp do_in_english(number) when rem(number, 1_000) == 0,
+    do: "#{do_in_english(div(number, 1_000))} thousand"
 
-  defp do_in_english(number) when rem(number, 100) == 0 do
-    "#{do_in_english(div(number, 100))} hundred"
-  end
+  defp do_in_english(number) when rem(number, 100) == 0,
+    do: "#{do_in_english(div(number, 100))} hundred"
 
-  defp do_in_english(number) when number > 1_000_000_000 do
-    "#{do_in_english(number - rem(number, 1_000_000_000))} #{
-      do_in_english(rem(number, 1_000_000_000))
-    }"
-  end
+  defp do_in_english(number) when number > 1_000_000_000,
+    do:
+      "#{do_in_english(number - rem(number, 1_000_000_000))} #{
+        do_in_english(rem(number, 1_000_000_000))
+      }"
 
-  defp do_in_english(number) when number > 1_000_000 do
-    "#{do_in_english(number - rem(number, 1_000_000))} #{do_in_english(rem(number, 1_000_000))}"
-  end
+  defp do_in_english(number) when number > 1_000_000,
+    do:
+      "#{do_in_english(number - rem(number, 1_000_000))} #{do_in_english(rem(number, 1_000_000))}"
 
-  defp do_in_english(number) when number > 1_000 do
-    "#{do_in_english(number - rem(number, 1_000))} #{do_in_english(rem(number, 1_000))}"
-  end
+  defp do_in_english(number) when number > 1_000,
+    do: "#{do_in_english(number - rem(number, 1_000))} #{do_in_english(rem(number, 1_000))}"
 
-  defp do_in_english(number) when number > 100 do
-    "#{do_in_english(number - rem(number, 100))} #{do_in_english(rem(number, 100))}"
-  end
+  defp do_in_english(number) when number > 100,
+    do: "#{do_in_english(number - rem(number, 100))} #{do_in_english(rem(number, 100))}"
 
-  defp do_in_english(number) when number > 20 do
-    "#{do_in_english(number - rem(number, 10))}-#{do_in_english(rem(number, 10))}"
-  end
+  defp do_in_english(number) when number > 20,
+    do: "#{do_in_english(number - rem(number, 10))}-#{do_in_english(rem(number, 10))}"
 end
