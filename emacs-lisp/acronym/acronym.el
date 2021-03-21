633e356eddee06137ccc3b8aaabdd81d5fcc614e
EmilienMottet <emilien.mottet@grenoble-inp.org>
2021-03-21 22:14:58 +0100

    reformat acronym

--- emacs-lisp/acronym/acronym.el
+++ emacs-lisp/acronym/acronym.el
@@ -7,12 +7,11 @@
 (require 'cl-lib)
 
 (defun acronym (str)
-  (upcase (mapconcat (lambda (s) (substring s
-                                            0
-                                            1))
-                     (split-string str
-                                   "[- \f\t\n\r\v]+")
-                     "")))
+  (upcase (mapconcat
+           (lambda (s)
+             (substring s 0 1))
+           (split-string str "[- \f\t\n\r\v]+")
+           "")))
 
 (provide 'acronym)
 ;;; acronym.el ends here
